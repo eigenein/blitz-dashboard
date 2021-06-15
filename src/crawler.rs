477d966b15f2204363e0530589568d9d82535f55
Pavel Perestoronin <eigenein@gmail.com>
2021-06-15 21:23:46 +0200

    ⚡️ Check account last battle time to avoid unnecessary API calls and database queries

--- src/crawler.rs
+++ src/crawler.rs
@@ -1,8 +1,8 @@
-use std::time::Instant;
+use std::time::{Duration as StdDuration, Instant};
 
 use anyhow::anyhow;
 use async_std::task::sleep;
-use chrono::{DateTime, Duration, Utc};
+use chrono::{Duration, Utc};
 
 use crate::database::Database;
 use crate::wargaming::WargamingApi;
@@ -11,16 +11,10 @@ const ACCOUNT_STALE_TIMEOUT_SECS: i64 = 60;
 
 pub async fn run(api: WargamingApi, database: Database) -> crate::Result {
     loop {
-        let account = database.retrieve_oldest_account()?;
-        let account = match account {
-            None => {
-                log::info!("No accounts in the database.");
-                sleep(std::time::Duration::from_secs(60)).await;
-                continue;
-            }
-            Some(account) => account,
-        };
-        let age = Utc::now() - Into::<DateTime<Utc>>::into(account.crawled_at);
+        let account = database
+            .retrieve_oldest_account()?
+            .ok_or_else(|| anyhow!("the database is empty"))?;
+        let age = Utc::now() - account.crawled_at;
         log::info!(
             "Selected account #{}, last crawled at {}.",
             account.id,
@@ -29,26 +23,27 @@ pub async fn run(api: WargamingApi, database: Database) -> crate::Result {
         let sleep_duration = (Duration::seconds(ACCOUNT_STALE_TIMEOUT_SECS) - age).num_seconds();
         if sleep_duration > 0 {
             log::info!("Sleeping for {} secs…", sleep_duration);
-            sleep(std::time::Duration::from_secs(
-                sleep_duration.unsigned_abs(),
-            ))
-            .await;
+            sleep(StdDuration::from_secs(sleep_duration.unsigned_abs())).await;
         }
 
         let start_instant = Instant::now();
-
         let account_info = api
             .get_account_info(account.id)
             .await?
             .ok_or_else(|| anyhow!("account #{} not found", account.id))?;
-        let tanks = api.get_merged_tanks(account.id).await?;
-
         let tx = database.start_transaction()?;
         database.upsert_account(&account_info.basic)?;
-        database.upsert_account_snapshot(&account_info)?;
-        database.upsert_tanks(&tanks)?;
+        if account_info.basic.last_battle_time > account.crawled_at {
+            database.upsert_account_snapshot(&account_info)?;
+            database.upsert_tanks(&api.get_merged_tanks(account.id).await?)?;
+        } else {
+            log::info!(
+                "Account #{} haven't played since {}.",
+                account.id,
+                account.crawled_at
+            );
+        }
         tx.commit()?;
-
         log::info!(
             "Account #{} crawled in {:?}.",
             account.id,
