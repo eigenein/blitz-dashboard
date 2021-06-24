bda6e8c6d6f5176b03b02d3c1d0fdc57879fd2e7
Pavel Perestoronin <eigenein@gmail.com>
2021-06-24 21:30:06 +0200

    ✨ Add new statistics fields

--- src/models.rs
+++ src/models.rs
@@ -58,6 +58,12 @@ pub struct AllStatistics {
     pub damage_received: i32,
     pub shots: i32,
     pub hits: i32,
+
+    #[serde(default)]
+    pub frags: i32,
+
+    #[serde(default)]
+    pub xp: i32,
 }
 
 #[derive(Deserialize, Debug, PartialEq, Clone)]
@@ -167,11 +173,22 @@ mod tests {
     type ResultMapVec<T> = ResultMap<Vec<T>>;
 
     #[test]
-    fn account_infos_ok() -> crate::Result {
+    fn account_info_map_ok() -> crate::Result {
         serde_json::from_str::<ResultMap<AccountInfo>>(
             // language=json
             r#"{"5589968":{"statistics":{"clan":{"spotted":0,"max_frags_tank_id":0,"hits":0,"frags":0,"max_xp":0,"max_xp_tank_id":0,"wins":0,"losses":0,"capture_points":0,"battles":0,"damage_dealt":0,"damage_received":0,"max_frags":0,"shots":0,"frags8p":0,"xp":0,"win_and_survived":0,"survived_battles":0,"dropped_capture_points":0},"all":{"spotted":5154,"max_frags_tank_id":20817,"hits":48542,"frags":5259,"max_xp":1917,"max_xp_tank_id":54289,"wins":3425,"losses":2609,"capture_points":4571,"battles":6056,"damage_dealt":6009041,"damage_received":4524728,"max_frags":6,"shots":63538,"frags8p":1231,"xp":4008483,"win_and_survived":2524,"survived_battles":2635,"dropped_capture_points":4415},"frags":null},"account_id":5589968,"created_at":1415225091,"updated_at":1621792747,"private":null,"last_battle_time":1621802244,"nickname":"eigenein"}}"#,
         )?;
+        Ok(())
+    }
+
+    #[test]
+    fn account_info_ok() -> crate::Result {
+        let info: AccountInfo = serde_json::from_str(
+            // language=json
+            r#"{"statistics":{"clan":{"spotted":0,"max_frags_tank_id":0,"hits":0,"frags":0,"max_xp":0,"max_xp_tank_id":0,"wins":0,"losses":0,"capture_points":0,"battles":0,"damage_dealt":0,"damage_received":0,"max_frags":0,"shots":0,"frags8p":0,"xp":0,"win_and_survived":0,"survived_battles":0,"dropped_capture_points":0},"all":{"spotted":5154,"max_frags_tank_id":20817,"hits":48542,"frags":5259,"max_xp":1917,"max_xp_tank_id":54289,"wins":3425,"losses":2609,"capture_points":4571,"battles":6056,"damage_dealt":6009041,"damage_received":4524728,"max_frags":6,"shots":63538,"frags8p":1231,"xp":4008483,"win_and_survived":2524,"survived_battles":2635,"dropped_capture_points":4415},"frags":null},"account_id":5589968,"created_at":1415225091,"updated_at":1621792747,"private":null,"last_battle_time":1621802244,"nickname":"eigenein"}"#,
+        )?;
+        assert_ne!(info.statistics.all.frags, 0);
+        assert_ne!(info.statistics.all.xp, 0);
         Ok(())
     }
 
