9bc7369067e2d4cd40cfb67716e3c4b2425bb09e
Pavel Perestoronin <eigenein@gmail.com>
2021-07-06 22:58:01 +0200

    ✨ Add quick links to some accounts, resolves #26

--- src/web/index.rs
+++ src/web/index.rs
@@ -19,13 +19,30 @@ pub async fn get(request: tide::Request<State>) -> tide::Result {
                     title { "Я статист!" }
                 }
                 body {
-                    section class="hero is-fullheight" {
-                        div class="hero-body" {
-                            div class="container" {
-                                div class="columns" {
-                                    div class="column is-8 is-offset-2" {
+                    section.hero.is-fullheight {
+                        div.hero-body {
+                            div.container {
+                                div.columns {
+                                    div.column."is-8"."is-offset-2" {
                                         form action="/search" method="GET" {
                                             (account_search("is-medium", "", true))
+                                            div.field.is-grouped.is-grouped-centered.is-grouped-multiline {
+                                                p.control {
+                                                    a.button.is-rounded.is-small href="/ru/3851977" { "D_W_S" }
+                                                }
+                                                p.control {
+                                                    a.button.is-rounded.is-small href="/ru/5303075" { "Perfect_M1nd" }
+                                                }
+                                                p.control {
+                                                    a.button.is-rounded.is-small href="/ru/4435872" { "_n0_skill_just_luck_" }
+                                                }
+                                                p.control {
+                                                    a.button.is-rounded.is-small href="/ru/2992069" { "Tortik" }
+                                                }
+                                                p.control {
+                                                    a.button.is-rounded.is-small href="/ru/103809874" { "Invincible_Beast" }
+                                                }
+                                            }
                                         }
                                     }
                                 }
