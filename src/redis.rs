dce51e3eb33442cf7010c867bd1482d9c3d5d210
Pavel Perestoronin <eigenein@gmail.com>
2021-09-09 19:15:20 +0200

    💥 Use `cf::vehicles` instead of the old keys #29

--- src/redis.rs
+++ src/redis.rs
@@ -10,11 +10,13 @@ pub async fn open(uri: &str) -> crate::Result<ConnectionManager> {
         .context("failed to connect to Redis")?)
 }
 
+const VEHICLE_FACTORS_KEY: &str = "cf::vehicles";
+
 pub async fn get_vehicle_factors(
     redis: &mut ConnectionManager,
     tank_id: i32,
 ) -> crate::Result<Vec<f64>> {
-    let value: Option<Vec<u8>> = redis.get(&get_vehicle_factors_key(tank_id)).await?;
+    let value: Option<Vec<u8>> = redis.hget(VEHICLE_FACTORS_KEY, tank_id).await?;
     match value {
         Some(value) => Ok(rmp_serde::from_read_ref(&value)?),
         None => Ok(Vec::new()),
@@ -26,21 +28,8 @@ pub async fn set_vehicle_factors(
     tank_id: i32,
     factors: &[f64],
 ) -> crate::Result {
-    // TODO: remove when `cf::vehicles` is populated.
     redis
-        .set(
-            &get_vehicle_factors_key(tank_id),
-            rmp_serde::to_vec(factors)?,
-        )
-        .await?;
-
-    redis
-        .hset("cf::vehicles", tank_id, rmp_serde::to_vec(factors)?)
+        .hset(VEHICLE_FACTORS_KEY, tank_id, rmp_serde::to_vec(factors)?)
         .await?;
     Ok(())
-}
-
-#[deprecated]
-fn get_vehicle_factors_key(tank_id: i32) -> String {
-    format!("t:{}:factors", tank_id)
 }
