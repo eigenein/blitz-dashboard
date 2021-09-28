cb302bc710d3b0c69e1edb6b517d023fbb2aeb9e
Pavel Perestoronin <eigenein@gmail.com>
2021-09-28 21:54:59 +0200

    🚧 Implement `Vector::cosine_similarity`

--- src/trainer/vector.rs
+++ src/trainer/vector.rs
@@ -12,7 +12,34 @@ impl Deref for Vector {
 
 impl Vector {
     #[must_use]
-    pub fn dot(&self, other: &Vector) -> f64 {
+    pub fn dot(&self, other: &Self) -> f64 {
         self.0.iter().zip(&other.0).map(|(xi, yi)| xi * yi).sum()
+    }
+
+    #[must_use]
+    pub fn cosine_similarity(&self, other: &Self) -> f64 {
+        let (dot, x_len_squared, y_len_squared) = self.0.iter().zip(&other.0).fold(
+            (0.0, 0.0, 0.0),
+            |(dot, x_len_squared, y_len_squared), (xi, yi)| {
+                (
+                    dot + xi * yi,
+                    x_len_squared + xi * xi,
+                    y_len_squared + yi * yi,
+                )
+            },
+        );
+        dot / x_len_squared.sqrt() / y_len_squared.sqrt()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cosine_similarity_ok() {
+        let vector_1 = Vector(vec![1.0, 2.0, 3.0]);
+        let vector_2 = Vector(vec![3.0, 5.0, 7.0]);
+        assert!((vector_1.cosine_similarity(&vector_2) - 0.9974149030430578).abs() < f64::EPSILON);
     }
 }
