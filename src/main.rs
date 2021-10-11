94cbc0f5d232ccc81d06b078998620f9ad198746
Pavel Perestoronin <eigenein@gmail.com>
2021-10-12 01:12:12 +0200

    ⏪ Roll back to the default allocator

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,6 @@ mod trainer;
 mod wargaming;
 mod web;
 
-#[global_allocator]
-static ALLOCATOR: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
 const CRATE_NAME: &str = env!("CARGO_PKG_NAME");
 const CRATE_VERSION: &str = env!("CARGO_PKG_VERSION");
 
