4d95c4b664dcc3d1683638e3b54a668a73515ffe
Pavel Perestoronin <eigenein@gmail.com>
2021-12-15 20:10:14 +0100

    🍻 Implement `impl FromRedisValue for SamplePoint`

--- src/trainer/sample_point.rs
+++ src/trainer/sample_point.rs
@@ -1,4 +1,5 @@
 use chrono::Utc;
+use redis::{ErrorKind, FromRedisValue, RedisError, RedisResult, Value};
 use serde::{Deserialize, Serialize};
 
 use crate::DateTime;
@@ -14,4 +15,39 @@ pub struct SamplePoint {
 
     #[serde(default = "Utc::now")]
     pub timestamp: DateTime,
+}
+
+impl FromRedisValue for SamplePoint {
+    fn from_redis_value(value: &Value) -> RedisResult<Self> {
+        let mut map = value.as_map_iter().ok_or_else(|| {
+            RedisError::from((
+                ErrorKind::TypeError,
+                "expected a map-compatible type",
+                format!("{:?}", value),
+            ))
+        })?;
+        let (_, value) = map.next().ok_or_else(|| {
+            RedisError::from((
+                ErrorKind::TypeError,
+                "expected a non-empty map",
+                format!("{:?}", value),
+            ))
+        })?;
+        let value = if let Value::Data(value) = value {
+            value
+        } else {
+            return Err(RedisError::from((
+                ErrorKind::TypeError,
+                "expected a binary data",
+                format!("{:?}", value),
+            )));
+        };
+        rmp_serde::from_read_ref(value).map_err(|error| {
+            RedisError::from((
+                ErrorKind::TypeError,
+                "failed to deserialize",
+                format!("{:?}", error),
+            ))
+        })
+    }
 }
