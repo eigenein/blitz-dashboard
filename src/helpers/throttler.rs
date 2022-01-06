7605e5977b4514a9ba78689e954e05f53c9d4106
Pavel Perestoronin <eigenein@gmail.com>
2022-01-06 23:55:24 +0100

    🚑 Crawler: I'm still trying to fix the throttler on Linux

--- src/helpers/throttler.rs
+++ src/helpers/throttler.rs
@@ -1,7 +1,8 @@
 use std::sync::Arc;
+use std::time::Instant;
 
 use tokio::sync::Mutex;
-use tokio::time::{sleep_until, Instant};
+use tokio::time::sleep;
 
 use crate::StdDuration;
 
@@ -28,7 +29,9 @@ impl Throttler {
 
         if guard.1 >= self.limit {
             let deadline = guard.0 + self.period;
-            sleep_until(deadline).await;
+            if let Some(duration) = deadline.checked_duration_since(Instant::now()) {
+                sleep(duration).await;
+            }
             *guard = (deadline, 1);
         } else {
             guard.1 += 1;
