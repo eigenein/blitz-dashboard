e2bcc31bef870170b85d2c2735d4de94569c79bb
Pavel Perestoronin <eigenein@gmail.com>
2022-07-08 19:18:42 +0200

    ♻️ Web: extract `Static`

--- src/web/views/static.rs
+++ src/web/views/static.rs
@@ -1,136 +1,126 @@
 use poem::{handler, IntoResponse};
 
-const CACHE_CONTROL: &str = "public, max-age=31536000, immutable";
+struct Static(&'static str, &'static [u8]);
 
+impl IntoResponse for Static {
+    #[inline]
+    fn into_response(self) -> poem::Response {
+        self.1
+            .with_content_type(self.0)
+            .with_header("Cache-Control", "public, max-age=31536000, immutable")
+            .into_response()
+    }
+}
+
+#[inline]
 #[handler]
 pub async fn get_site_manifest() -> impl IntoResponse {
-    include_bytes!("static/site.webmanifest")
-        .with_content_type("application/json")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("application/json", include_bytes!("static/site.webmanifest"))
 }
 
 #[handler]
 pub async fn get_favicon() -> impl IntoResponse {
-    include_bytes!("static/favicon.ico")
-        .with_content_type("image/vnd.microsoft.icon")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/vnd.microsoft.icon", include_bytes!("static/favicon.ico"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_favicon_16x16() -> impl IntoResponse {
-    include_bytes!("static/favicon-16x16.png")
-        .with_content_type("image/png")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/png", include_bytes!("static/favicon-16x16.png"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_favicon_32x32() -> impl IntoResponse {
-    include_bytes!("static/favicon-32x32.png")
-        .with_content_type("image/png")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/png", include_bytes!("static/favicon-32x32.png"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_android_chrome_192x192() -> impl IntoResponse {
-    include_bytes!("static/android-chrome-192x192.png")
-        .with_content_type("image/png")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/png", include_bytes!("static/android-chrome-192x192.png"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_android_chrome_512x512() -> impl IntoResponse {
-    include_bytes!("static/android-chrome-512x512.png")
-        .with_content_type("image/png")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/png", include_bytes!("static/android-chrome-512x512.png"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_apple_touch_icon() -> impl IntoResponse {
-    include_bytes!("static/apple-touch-icon.png")
-        .with_content_type("image/png")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/png", include_bytes!("static/apple-touch-icon.png"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_table_js() -> impl IntoResponse {
-    include_bytes!("static/table.js")
-        .with_content_type("application/javascript")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("application/javascript", include_bytes!("static/table.js"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_theme_css() -> impl IntoResponse {
-    include_bytes!("static/theme.css")
-        .with_content_type("text/css")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("text/css", include_bytes!("static/theme.css"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_robots_txt() -> impl IntoResponse {
-    include_bytes!("static/robots.txt")
-        .with_content_type("text/plain")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("text/plain", include_bytes!("static/robots.txt"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_cn_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/cn.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/cn.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_de_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/de.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/de.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_eu_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/eu.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/eu.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_fr_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/fr.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/fr.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_gb_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/gb.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/gb.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_jp_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/jp.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/jp.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_su_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/su.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/su.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_us_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/us.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/us.svg"))
 }
 
+#[inline]
 #[handler]
 pub async fn get_xx_svg() -> impl IntoResponse {
-    include_bytes!("static/flags/xx.svg")
-        .with_content_type("image/svg+xml")
-        .with_header("Cache-Control", CACHE_CONTROL)
+    Static("image/svg+xml", include_bytes!("static/flags/xx.svg"))
 }
