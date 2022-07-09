e8742e4a0c95f12a74f3e98e4d9477bc8cc36e7b
Pavel Perestoronin <eigenein@gmail.com>
2022-07-09 13:20:15 +0200

    🐛 Web: fix the error when search query is omitted #YASTATIST-4E

--- src/web/views/search/models.rs
+++ src/web/views/search/models.rs
@@ -8,6 +8,7 @@ pub const MAX_QUERY_LENGTH: usize = 24;
 
 #[derive(Deserialize, Validate)]
 pub struct Params {
+    #[serde(default)]
     #[validate(length(min = "MIN_QUERY_LENGTH", max = "MAX_QUERY_LENGTH"))]
     pub query: String,
 
