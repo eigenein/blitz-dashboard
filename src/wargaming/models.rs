b8c13b009385c110514478c82923384adaf5ce3a
Pavel Perestoronin <eigenein@gmail.com>
2022-07-13 21:26:43 +0200

    🚧 Splitting `tanks_stats`

--- src/wargaming/models.rs
+++ src/wargaming/models.rs
@@ -1,20 +1,23 @@
-pub use account_id::*;
-pub use account_info::*;
-pub use nation::*;
-pub use realm::*;
 use serde::Deserialize;
-pub use statistics::*;
-pub use tank_id::*;
-pub use tank_statistics::*;
-pub use vehicle::*;
+
+pub use self::account_id::*;
+pub use self::account_info::*;
+pub use self::nation::*;
+pub use self::realm::*;
+pub use self::statistics::*;
+pub use self::tank_achievements::*;
+pub use self::tank_id::*;
+pub use self::tank_stats::*;
+pub use self::vehicle::*;
 
 pub mod account_id;
 pub mod account_info;
 pub mod nation;
 pub mod realm;
 pub mod statistics;
+pub mod tank_achievements;
 pub mod tank_id;
-pub mod tank_statistics;
+pub mod tank_stats;
 pub mod vehicle;
 
 /// Search accounts item.
