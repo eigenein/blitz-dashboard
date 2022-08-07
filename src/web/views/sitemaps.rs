02577be49d88ca78fc5d23a3e9bd13a471a7d430
Pavel Perestoronin <eigenein@gmail.com>
2022-08-07 16:54:10 +0200

    🔍️ Web: reduce the sitemaps to 1000 entries

--- src/web/views/sitemaps.rs
+++ src/web/views/sitemaps.rs
@@ -5,7 +5,7 @@ use poem::{handler, Body, IntoResponse, Response};
 use crate::database;
 use crate::prelude::*;
 
-const CACHE_CONTROL: &str = "public, max-age=3600, stale-while-revalidate=86400";
+const CACHE_CONTROL: &str = "no-cache";
 
 #[handler]
 #[instrument(skip_all, level = "info")]
@@ -13,15 +13,16 @@ pub async fn get_sitemap(
     db: Data<&mongodb::Database>,
     Path(realm): Path<String>,
 ) -> Result<impl IntoResponse> {
-    let stream = database::AccountEntry::retrieve_page(&db, &realm, 50000)
-        .await?
-        .map(move |account| {
-            account.map(|account| {
-                format!("https://yastati.st/{}/{}\n", realm, account.id)
-                    .as_bytes()
-                    .to_vec()
-            })
-        });
+    let start_instant = Instant::now();
+    let stream = database::AccountEntry::retrieve_page(&db, &realm, 1000).await?;
+    info!(elapsed_secs = ?start_instant.elapsed(), "stream ready");
+    let stream = stream.map(move |account| {
+        account.map(|account| {
+            format!("https://yastati.st/{}/{}\n", realm, account.id)
+                .as_bytes()
+                .to_vec()
+        })
+    });
 
     Ok(Response::from(Body::from_bytes_stream(stream))
         .with_header("Cache-Control", CACHE_CONTROL)
