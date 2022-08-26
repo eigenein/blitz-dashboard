a525ffc84e0487d720e19a87cd700aacff64e41f
Pavel Perestoronin <eigenein@gmail.com>
2022-08-26 16:36:24 +0200

    🚧 Math: add `logit` and `sigmoid` – for the future, just in case

--- src/math.rs
+++ src/math.rs
@@ -1,2 +1,14 @@
 pub mod statistics;
 pub mod traits;
+
+#[allow(dead_code)]
+#[inline]
+pub fn logit(p: f64) -> f64 {
+    (p / (1.0 - p)).ln()
+}
+
+#[allow(dead_code)]
+#[inline]
+pub fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
+}
