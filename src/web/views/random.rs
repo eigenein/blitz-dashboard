28aaf1fe2872dedf20913709267ad9281a214468
Pavel Perestoronin <eigenein@gmail.com>
2022-09-07 02:22:41 +0200

    ➖ Drop `fastrand`

--- src/web/views/random.rs
+++ src/web/views/random.rs
@@ -1,5 +1,6 @@
 use poem::web::{Data, Redirect};
 use poem::{handler, IntoResponse};
+use rand::prelude::*;
 
 use crate::prelude::*;
 use crate::{database, wargaming};
@@ -7,7 +8,9 @@ use crate::{database, wargaming};
 #[handler]
 #[instrument(skip_all, level = "info")]
 pub async fn get_random(db: Data<&mongodb::Database>) -> Result<impl IntoResponse> {
-    let realm = [wargaming::Realm::Russia, wargaming::Realm::Europe][fastrand::usize(0..2)];
-    let account = database::Account::sample_account(*db, realm).await?;
+    let realm = [wargaming::Realm::Russia, wargaming::Realm::Europe]
+        .choose(&mut thread_rng())
+        .unwrap();
+    let account = database::Account::sample_account(*db, *realm).await?;
     Ok(Redirect::temporary(format!("/{}/{}", realm.to_str(), account.id)))
 }
